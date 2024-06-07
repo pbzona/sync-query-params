66dbfd0fa26f7e94db017be9fe7d4da8fecfd674
Phil Zona <phil.zona@vercel.com>
2024-06-07 11:41:34 -0400

    Make delay more obvious

--- app/api/route.ts
+++ app/api/route.ts
@@ -3,7 +3,7 @@ export const dynamic = 'force-dynamic';
 export async function GET(req: Request) {
   const searchParams = new URL(req.url).searchParams;
 
-  const latency = Math.random() + 1; // In seconds
+  const latency = Math.random() * 2 + 1; // In seconds
   await new Promise(r => setTimeout(r, latency * 1000));
 
   return Response.json({
